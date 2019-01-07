7f4ea166e62b798bea2a64b05a964c70f2c8d413
Daniel Roesch <darmbc@mst.edu>
2019-01-07 12:54:34 -0600

    Made the process and output fields work

--- CountingCharacters/Data Structures/Program.cs
+++ CountingCharacters/Data Structures/Program.cs
@@ -10,17 +10,32 @@ namespace Data_Structures
         {
             //input a string and count all charaters in it using dictionary
             //varables
-            Dictionary<char, long> charCount = new Dictionary<char, long>();
-            string input = "";
+            Dictionary<char, long> charCounts = new Dictionary<char, long>();
+            //string input = "";
+            string input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
 
-            //input
+            /*//input
             Console.WriteLine("Please input the desired string to be tested: ");
-            input = Console.ReadLine();
+            input = Console.ReadLine();*/
 
             //process
-
+            foreach (char letter in input)
+            {
+                if (charCounts.ContainsKey(letter))
+                {
+                    charCounts[letter] = charCounts[letter] + 1;
+                }
+                else
+                {
+                    charCounts.Add(letter, 1);
+                }
+            }
 
             //output
+            foreach (KeyValuePair<char,long> charCount in charCounts)
+            {
+                Console.WriteLine(charCount.Key + ": " + charCount.Value);
+            }
         }
     }
 }
