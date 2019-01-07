da12a710beb0b39bfc53f754097987d4d783307c
Daniel Roesch <darmbc@mst.edu>
2019-01-06 22:31:40 -0600

    Main program made

---
+++ AreaOfACircle/AreaOfACirle/AreaOfACirle/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace AreaOfACirle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Varables
+            double radius = 0, area = 0;
+            //input a radius and output the area
+            Console.WriteLine("Please enter the radius of a circle: ");
+            radius = double.Parse (Console.ReadLine());
+
+            //Calculate the area
+            area = Math.PI * radius * radius;
+
+            //Output
+            Console.WriteLine("The area of a circle with radius " + radius + " is: " + area);
+        }
+    }
+}
