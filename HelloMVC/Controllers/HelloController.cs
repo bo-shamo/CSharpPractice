ef329f441ec2391514bbdf27ed3bbdf7d8c951f9
Daniel Roesch <darmbc@mst.edu>
2019-02-24 22:22:57 -0600

    Worked with views to get controller and views working together

---
+++ HelloMVC/Controllers/HelloController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string html = "<form method = 'post' >" +
+                 "<input type='text' name = 'name' />" +
+                 "<label for = 'lang'> Choose a Language:</label>" +
+                 "<select id='lang' name = 'lang'>" +
+                 "<option value = ''> --Please Choose a Language--</option>" +
+                 "<option value = 'english'>English</option>" +
+                "<option value = 'french'>French</option>" +
+                "<option value = 'german'>German</option>" +
+                "<option value = 'spanish'>Spanish</option>" +
+                "<option value = 'japanese'>Japanese</option>" +
+                "</select>" +
+                 "<input type='submit' value='Greet me!' />" +
+                 "</form>";
+
+            return Content(html,"text/html");
+        }
+
+        // /Hello
+        [Route("/Hello")]
+        [HttpPost]
+        public IActionResult Display(string name = "World", string lang = "english")
+        {
+            string greeting;
+            switch (lang)
+            {
+                case "french":
+                    greeting = "Bonjour";
+                    break;
+                case "german":
+                    greeting = "Guten Tag";
+                    break;
+                case "spanish":
+                    greeting = "Hola";
+                    break;
+                case "japanese":
+                    greeting = "Kon'nichiwa";
+                    break;
+                default:
+                    greeting = "Hello";
+                    break;
+            }
+            return Content(string.Format("<h1>{0} {1}</h1>", greeting, name), "text/html");
+        }
+
+        //Handle requests to /Hello/Chris (URL segement)
+        [Route("/Hello/{name}")]
+        public IActionResult Index2(string name)
+        {
+            return Content(string.Format("<h1>Hello {0}</h1>", name), "text/html");
+        }
+
+        
+
+        public IActionResult Goodbye()
+        {
+            return Content("Goodbye");
+        }
+    }
+}
---
+++ Views/CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+
+        static private List<String> Cheeses = new List<string>();
+        // GET: /<controller>/
+
+
+        public IActionResult Index()
+        {
+
+            /*cheeses.Add("Cheddar");
+            cheeses.Add("Munster");
+            cheeses.Add("Paremsan");*/
+
+            ViewBag.cheeses = Cheeses;
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Cheese/Add")]
+        public IActionResult NewCheese(string name)
+        {
+            // Add new cheese to cheese
+            Cheeses.Add(name);
+
+            return Redirect("/Cheese");
+
+        }
+    }
+}
